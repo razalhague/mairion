4607695089db4548360022a015b4cf8f95343700
Ville Jokela <ville.jokela@penny-craal.org>
2017-04-11 15:35:06 +0300

    refactor turning a List into an Optional

--- src/main/java/org/penny_craal/mairion/model/UserDao.java
+++ src/main/java/org/penny_craal/mairion/model/UserDao.java
@@ -1,11 +1,11 @@
 package org.penny_craal.mairion.model;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.penny_craal.mairion.utils.Util;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,22 +19,15 @@ public class UserDao {
 	}
 
 	public Optional<User> getUser(int id) {
-		return getOptionalUser(em.createNamedQuery("User.selectById", User.class)
+		return Util.getOptionalFromList(em.createNamedQuery("User.selectById", User.class)
 				.setParameter("id", id)
 				.getResultList());
 	}
 
 	public Optional<User> getUser(String email) {
-		return getOptionalUser(em.createNamedQuery("User.selectByEmail", User.class)
+		return Util.getOptionalFromList(em.createNamedQuery("User.selectByEmail", User.class)
 				.setParameter("email", email)
 				.getResultList());
 	}
 
-	private Optional<User> getOptionalUser(List<User> users) {
-		if (users.isEmpty() || users.size() > 1) {
-			return Optional.empty();
-		} else { // there is exactly one result
-			return Optional.of(users.get(0));
-		}
-	}
 }
---
+++ src/main/java/org/penny_craal/mairion/utils/Util.java
@@ -0,0 +1,20 @@
+package org.penny_craal.mairion.utils;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Util {
+	/**
+	 * Tries to get exactly one value from a list.
+	 * @param list list of values
+	 * @param <T>
+	 * @return the sole value in the list, now in an Optional, or an empty Optional
+	 */
+	public static <T> Optional<T> getOptionalFromList(List<T> list) {
+		if (list.isEmpty() || list.size() > 1) {
+			return Optional.empty();
+		} else { // there is exactly one result
+			return Optional.of(list.get(0));
+		}
+	}
+}
